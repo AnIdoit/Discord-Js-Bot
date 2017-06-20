6cd5428b31545664a3fed8d33bd6306d5b4bf1d7
AnIdoit <zribss@gmail.com>
2017-06-20 01:33:33 -0700

    random command added

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Discord-Js-Bot
 The Discord bot I'm writing in JavaScript(Discord.js)
 
-[Join my discord](https://discord.gg/XYySGKz)
-
 - dependencies
   - "discord.js": "^11.1.0"
 
@@ -22,3 +20,9 @@ The Discord bot I'm writing in JavaScript(Discord.js)
     - guildinfo
   - less useful
     - ping
+    - random [number(1-10)/letter]
+
+### Servers
+<a href="https://discord.gg/XYySGKz"><img src="https://discordapp.com/api/guilds/324962753171161108/widget.png?style=banner2" alt="Discord server"></a>
+
+<a href="https://discord.gg/AyGxw76"><img src="https://discordapp.com/api/guilds/326513632123027456/widget.png?style=banner2" alt="Discord server"></a>
---
+++ commands/random.js
@@ -0,0 +1,17 @@
+exports.run = (client, message, args) => {
+  let config = require("../config");
+  if(message.content.startsWith(config.bot.prefix + random number)) {
+    let arrayNum = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+    let Random = arrayNum[Math.floor(Math.random() * arrayNum.length)];
+
+    message.channel.send(Random);
+  } else
+  if(message.content.startsWith(config.bot.prefix + random letter)) {
+    let arrayNum = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let Random = arrayNum[Math.floor(Math.random() * arrayNum.length)];
+
+    message.channel.send(Random);
+  } else {
+    message.channel.send(`${message.user.username}, random number or random letter...`)
+  }
+};
